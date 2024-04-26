fe1b4848c09202bb8501541830993f80a9e3f166
muhammadirtaza <87863581+MuhummadIrtaza@users.noreply.github.com>
2024-04-26 05:31:32 +0000

    downloadssorter.py

--- downloadssorter.py
+++ downloadssorter.py
@@ -1,23 +1,65 @@
 import os  
 import pathlib 
-path = "/home/irtaza/Downloads"
+import shutil
+
+path = input("Enter the path you want to organize the files: ")
 files = [f for f in os.listdir(path)]
-images= [".jpg" , ".png"]
 
 
 def make_folders():
-        os.makedirs(path + "/images")
-        os.makedirs(path + "/executables")
-        os.makedirs(path + "/miscallaneous")
-        os.makedirs(path + "/music")
+    file_paths = ["/images" , "/executables" , "/miscallaneous" , "/music" , "/videos" , "/books" , "/compressed"]                                                                                                                                                                                                                                          
 
-for items in files:
-    try:
-        extention = pathlib.Path(items).suffix
-        print(extention)
+    for paths in file_paths:
+        if not os.path.exists(path + paths):
+            os.makedirs(path + paths)
 
-    except:
-        print("lol")
+def sort():
+
+    imgs = [".jpg", ".png", ".gif", ".bmp", ".tiff"]
+    execs = [".exe", ".com", ".bat", ".msi", "..app"]
+    videos = [".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".mpeg", ".m4v", ".webm", ".rmvb"]
+    music = [".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma", ".m4a", ".ape", ".alac", ".midi"]
+    books = [".pdf" , ".epub"]
+    compressed = [".zip", ".rar", ".7z", ".tar", ".gzip", ".bz2", ".xz", ".tar.gz", ".tar.bz2", ".tar.xz", ".gz", ".bz2", ".xz"]
+
+    for items in files:
+        extensions = pathlib.Path(items).suffix
+        src_path = path + "/" + items
+
+        # move extensions of images to images file
+        if extensions in imgs:
+            dst_path = path + "/images"
+            shutil.move(src_path , dst_path)
+            
+        #move extension of executables to executables file
+        elif extensions in execs:
+            dst_path = path + "/executables"
+            shutil.move(src_path , dst_path)
+
+        #move music files
+        elif extensions in music:
+            dst_path = path + "/music"
+            shutil.move(src_path , dst_path)
+
+        #move compressed files
+        elif extensions in compressed:
+            dst_path = path + "/compressed"
+            shutil.move(src_path , dst_path)
+
+        elif extensions in books:
+            dst_path = path + "/books"
+            shutil.move(src_path , dst_path)
+
+        #move miscallaneous
+        else:
+            dst_path  = path + "/miscallaneous"
+            if not os.path.exists(os.path.join(dst_path, items)):
+                shutil.move(src_path, dst_path)
 
 
-make_folders()
+
+
+
+if __name__ == "__main__":
+    make_folders()
+    sort()
